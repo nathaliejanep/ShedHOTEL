74e0196fa7d8381df63dbb6fd5f0ee91afcd28e1
nathaliejanep <nathalie.persson@hotmail.com>
2023-01-31 14:05:32 +0100

    Contact html done

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
-
+import './Contact.scss';
+import contactHero from '../../assets/images/contact.jpg';
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="contact">
+      <section className="hero">
+        <img className="hero-image" src={contactHero} alt="People talking" />
+      </section>
+
+      <div className="container">
+        <section className="container__section">
+          <h1>Contact</h1>
+          <p>
+            About us. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Donec elit ligula, laoreet et arcu at, dignissim blandit ligula.
+            Suspendisse elementum tristique est, eget pulvinar tellus tristique
+            at.
+          </p>
+          <p>
+            <a href="mailto:info@shedhotels.com">info@shedhotels.com</a>
+          </p>
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
