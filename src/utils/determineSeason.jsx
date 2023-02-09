f545b69f1d9d32640924a1533824765c7e83b599
nathaliejanep <nathalie.persson@hotmail.com>
2023-02-09 13:08:27 +0100

    Changed months and added comments

--- src/utils/determineSeason.jsx
+++ src/utils/determineSeason.jsx
@@ -4,11 +4,16 @@ export const determineSeason = () => {
 
   const month = date.getMonth();
 
-  if (month <= 3) {
+  // January - April
+  if (month <= 4) {
     winter = true;
-  } else if (month >= 10 && month <= 11) {
+
+    // October - December
+  } else if (month >= 10 && month <= 12) {
     winter = true;
-  } else if (month >= 4 && month <= 9) {
+
+    // May - September
+  } else if (month >= 5 && month <= 9) {
     //summer
     winter = false;
   }
