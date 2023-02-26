692bc146a992bb4de4c53b7460006f8c8195a7e0
nathaliejanep <nathalie.persson@hotmail.com>
2023-02-26 22:22:54 +0100

    Last touch ups

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Button.scss';
+
+const Button = () => {
+  return (
+    <a
+      href="https://sky-eu1.clock-software.com/spa/pms-wbe/#/hotel/12633"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="green-btn"
+    >
+      Reserve
+    </a>
+  );
+};
+
+export default Button;
