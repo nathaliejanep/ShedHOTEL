9f0261f83a6ceb0271fb322322ee265c36ba3448
nathaliejanep <nathalie.persson@hotmail.com>
2023-05-02 10:44:52 +0200

    Changed opening hours

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import './Banner.scss';
 
 const Banner = () => {
-  return (
-    <div className="banner">
-      <p className="banner__text">
+    return (
+        <div className='banner'>
+            <p className='banner__text'>
+                {/* open monday - sunday 18.00 - late . open monday - sunday 18.00 - late .
         open monday - sunday 18.00 - late . open monday - sunday 18.00 - late .
-        open monday - sunday 18.00 - late . open monday - sunday 18.00 - late .
-        open monday - sunday 18.00 - late . open monday - sunday 18.00 - late .
-      </p>
-    </div>
-  );
+        open monday - sunday 18.00 - late . open monday - sunday 18.00 - late . */}
+                closed for the season, back in the summer // closed for the season, back in the summer // closed for the season, back in the
+                summer // closed for the season, back in the summer // closed for the season, back in the summer //
+            </p>
+        </div>
+    );
 };
 
 export default Banner;
